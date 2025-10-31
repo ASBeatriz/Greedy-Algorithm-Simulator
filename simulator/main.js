157e749be234fe901e68725b975f333c345f0ea9
Beatriz <beatriz.alvessantos05@gmail.com>
2025-10-31 12:55:57 -0300

    arruma identação

--- simulator/main.js
+++ simulator/main.js
@@ -2,21 +2,21 @@ const botao = document.getElementById("botaoSai");
 const filaContainer = document.getElementById("fila-container");
 
 function removerPessoa() {
-  const pessoas = filaContainer.querySelectorAll(".pessoa");
-  
-  if (pessoas.length > 0) {
-    // Remove a primeira pessoa
-    const primeira = pessoas[0];
-    primeira.style.opacity = "0";
-    primeira.style.transform = "translateY(-20px)";
-    
-    // Espera a animação e depois remove do DOM
-    setTimeout(() => {
-      primeira.remove();
-    }, 400);
-  } else {
-    alert("A fila está vazia!");
-  }
+    const pessoas = filaContainer.querySelectorAll(".pessoa");
+
+    if (pessoas.length > 0) {
+        // Remove a primeira pessoa
+        const primeira = pessoas[0];
+        primeira.style.opacity = "0";
+        primeira.style.transform = "translateY(-20px)";
+
+        // Espera a animação e depois remove do DOM
+        setTimeout(() => {
+            primeira.remove();
+        }, 400);
+    } else {
+        alert("A fila está vazia!");
+    }
 };
 
 
